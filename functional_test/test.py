9372237d178877d419f3d0d1da34c7c94e5d9a0e
denncbgit <cf19dennis.cabrera@iesjoandaustria.org>
2019-11-11 17:29:11 +0100

    First Functional Test

---
+++ functional_test/test.py
@@ -0,0 +1,10 @@
+"""
+	Functional test 
+	this file contains functional test for the logo functionality
+"""
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def test_find_logo_in_home_page(self):
+        assert False,"Do teh tetsing"
