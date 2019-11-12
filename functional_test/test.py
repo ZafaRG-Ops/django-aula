7f25a832cb6e9bc4532a62aa9b598dcf0f447e83
Andriy Zaviyskyy <andriy.zaviyskyy@iesjoandaustria.org>
2019-11-12 16:35:58 +0100

    First test specting logo

--- functional_test/test.py
+++ functional_test/test.py
@@ -1,10 +1,102 @@
 """
-	Functional test 
-	this file contains functional test for the logo functionality
+    Functional test 
+    this file contains functional test for the logo functionality
 """
-import unittest
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
 
-    def test_find_logo_in_home_page(self):
-        assert False,"Do the testing"
+class NewVisitorTest(LiveServerTestCase):
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_list_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+    def test_can_start_a_list_for_one_user(self):
+        # Marta has heard about a cool new online django-aula app. 
+        # She goes to check out its homepage. 
+        self.browser.get(self.live_server_url)
+        # Marta checks the logo is in homepage
+        # She tries to find a logo tag in homepage
+        logo_tags = self.browser.find_elements_by_css_selector('img#companyLogo')
+        self.assertEqual(len(logo_tags), 1)
+        self.fail('Finish the test!')
+
+
+
+
+
+
+        """
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        # self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        #self.fail('Finish the test!')
+
+        # She visits that URL - her to-do list is still there
+        # Satisfied, she goes back to sleep
+        """
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')  
+
